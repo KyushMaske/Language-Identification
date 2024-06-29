023a2635d08fa8515025cdcf979ab0217bf4da68
kyush <MAskeykyush@gmail.com>
2024-06-29 19:24:13 +0545

    Database Issue fixed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Language-Identification
 This repo consist of language identification  and change it to 'en'.
+
+Built a language translation model using Keras (trained for 5 epochs). The backend is implemented with FastAPI, containerized with Docker, and backed by a PostgreSQL database. A HTML template is used for the frontend.
+
+
+**uvicorn fastapi_app:app --reload**
--- database.py
+++ database.py
@@ -1,30 +1,30 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# # Replace these values with your PostgreSQL configuration
-# DATABASE_USER = "postgres"
-# DATABASE_PASSWORD = "admin"
-# DATABASE_HOST = "localhost"
-# DATABASE_PORT = "5432"
-# DATABASE_NAME = "languagetranslate"
-
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
-
-
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Replace these values with your PostgreSQL configuration
+DATABASE_USER = "postgres"
+DATABASE_PASSWORD = "admin"
+DATABASE_HOST = "localhost"
+DATABASE_PORT = "5432"
+DATABASE_NAME = "languagetranslate"
 
-engine = create_engine(DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+
+
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
